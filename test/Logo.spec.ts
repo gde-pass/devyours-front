0ddaaeab21fcd82f2cb29cf55c2573bf95f5e45a
Gregory <greg@coding-days.com>
2020-12-16 18:43:05 +0100

    refactor jest to typescript

--- test/Logo.spec.ts
+++ test/Logo.spec.ts
@@ -1,9 +1,13 @@
 import { mount } from '@vue/test-utils'
-import Logo from '@/components/Logo.vue'
+import Logo from '~/components/atoms/Logo.vue'
 
 describe('Logo', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Logo)
+    const wrapper = mount(Logo, {
+      stubs: {
+        NuxtLink: true,
+      },
+    })
     expect(wrapper.vm).toBeTruthy()
   })
 })
